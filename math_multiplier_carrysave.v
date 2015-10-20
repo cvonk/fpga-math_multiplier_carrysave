465e9d0a2269849ad0e8f1284b83de411f11e6f1
cvonk <coert.vonk@gmail.com>
2015-10-20 10:40:54 -0700

    rename files

--- math_multiplier_carrysave.v
+++ math_multiplier_carrysave.v
@@ -1,9 +1,12 @@
 `timescale 1ns / 10ps
+`default_nettype none
+// (c) 2015, by Coert Vonk
+// http://www.coertvonk.com/technology/logic/fpga-math-verilog-12758/5
 
-module multiplier #( parameter N = 4)
-                   ( input [N-1:0] a,
-                     input [N-1:0] b,
-                     output [N*2:0] p );
+module math_multiplier_carrysave #( parameter N = 4)
+									       ( input wire [N-1:0] a,
+									         input wire [N-1:0] b,
+									         output wire [N*2-1:0] p );
 							
    wire [N+1:0] s [N:0];
    wire [N+1:0] c [N:0];
@@ -13,7 +16,8 @@ module multiplier #( parameter N = 4)
 	   for ( ii = 0; ii <= N; ii = ii + 1) begin: gen_ii
          for ( jj = 0; jj < N; jj = jj + 1) begin: gen_jj
 
-            ma ma( .x  ( ii < N ? a[jj] : (jj > 0) ? c[4][jj-1] : 1'b0 ),
+            math_multiplier_ma_block ma(
+				       .x  ( ii < N ? a[jj] : (jj > 0) ? c[4][jj-1] : 1'b0 ),
                    .y  ( ii < N ? b[ii] : 1'b1 ),
                    .si ( ii > 0 && jj < N - 1 ? s[ii-1][jj+1] : 1'b0 ),
                    .ci ( ii > 0 ? c[ii-1][jj] : 1'b0 ),
@@ -25,7 +29,7 @@ module multiplier #( parameter N = 4)
 
 			assign p[ii] = s[ii][0];
        end
-		 assign p[N*2]  = c[N][N-1];
+		 // ignore the last carry, instead of doing assign p[N*2]  = c[N][N-1];
    endgenerate
 
 endmodule
